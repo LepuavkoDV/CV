b5a1e35890e7f4fdca1238ae6aba8cfbddce01be
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-06-13 19:29:50 +0300

    basic styling for pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="main-container">
     <Main-header></Main-header>
-    <Sidebar></Sidebar>
+    <Sidebar v-show="currentRoute !== 'pageNotFound'"></Sidebar>
     <router-view/>
   </div>
 </template>
@@ -9,11 +9,17 @@
 <script>
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
+import router from './router'
 export default {
   name: 'App',
   components: {
     'Main-header': Header,
     'Sidebar': Sidebar
+  },
+  data () {
+    return {
+      currentRoute: router.currentRoute.name
+    }
   }
 }
 </script>
