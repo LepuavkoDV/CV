aa4c8b42c000cc7c69257138cdf03af643d30d82
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-06-14 22:42:38 +0300

    connect-history-api-fallback added for production server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 let express = require('express')
 let path = require('path')
 let serveStatic = require('serve-static')
+var history = require('connect-history-api-fallback')
 let port = process.env.PORT || 5000
 
 const app = express()
+app.use(history())
 app.use(serveStatic(path.resolve('dist')))
 
 app.use(function (req, res, next) {
