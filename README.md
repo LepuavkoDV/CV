cd77164d2c0a5bd04706d0eb3b83223e271c2d76
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-06-15 14:42:49 +0300

    es6 support for server

--- README.md
+++ README.md
@@ -20,4 +20,8 @@ npm run build --report
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
+## Heroku configuration for using dev dependencies
+``` bash
+# heroku config:set NPM_CONFIG_PRODUCTION=false -a ldvcv
+
 [live](https://ldvcv.herokuapp.com/)
--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-let express = require('express')
-let path = require('path')
-let serveStatic = require('serve-static')
-var history = require('connect-history-api-fallback')
+import express from 'express'
+import { resolve } from 'path'
+import serveStatic from 'serve-static'
+import history from 'connect-history-api-fallback'
 let port = process.env.PORT || 5000
 
 const app = express()
 app.use(history())
-app.use(serveStatic(path.resolve('dist')))
+app.use(serveStatic(resolve('dist')))
 
 app.use(function (req, res, next) {
   res.status(404).send('Sorry cant find that!')
