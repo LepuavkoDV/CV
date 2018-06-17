cff2c2c7878bcfc88ea5281310f34ff3eeabba31
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-06-17 17:09:40 +0300

    cleanups & adds to previous commits

--- src/store/skills.js
+++ src/store/skills.js
@@ -12,18 +12,18 @@ const mutations = {
 
 const actions = {
   loadGroups: ({commit}) => {
-    return axios.get(process.env.API_ENDPOINT + '/groups').then(res => {
+    return axios.get(process.env.API_ENDPOINT + process.env.API_VERSION + '/groups').then(res => {
       commit('LOAD_GROUPS', res.data)
     })
   },
   addGroup: ({dispatch}, data) => {
     console.log(data)
-    return axios.post(process.env.API_ENDPOINT + '/group', data).then(res => {
+    return axios.post(process.env.API_ENDPOINT + process.env.API_VERSION + '/group', data).then(res => {
       dispatch('loadGroups')
     })
   },
   addSkill: ({dispatch}, data) => {
-    return axios.post(process.env.API_ENDPOINT + '/skill', data).then(res => {
+    return axios.post(process.env.API_ENDPOINT + process.env.API_VERSION + '/skill', data).then(res => {
       dispatch('loadGroups')
     })
   }
