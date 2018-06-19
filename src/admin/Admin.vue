67cdf0d35be53b29c7fdfd9c7227fe4934925b08
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-06-19 19:01:52 +0300

    deploy to Heroku

--- src/admin/Admin.vue
+++ src/admin/Admin.vue
@@ -1,61 +1,61 @@
 <template lang="html">
 
-  <section class="admin content">
-    <h1>admin Component</h1>
-    <div class="groups-crud">
-      <form v-on:submit.prevent="submitGroup">
-        <caption>Add group</caption>
+  <section class="admin content pl-5 pr-5">
+    <h2>admin Component</h2>
+    <div class="groups-crud pl-5 pr-5">
+      <form v-on:submit.prevent="submitGroup" class="mb-5">
+        <h4>Add group</h4>
         <div class="form-group">
           <label for="input">Group name:</label>
-          <input type="text" name="group-name" v-model="group.title">
+          <input class="form-control" type="text" name="group-name" v-model="group.title">
         </div>
         <div class="form-group">
           <label for="input">Icon:</label>
-          <input type="text" name="icon" v-model="group.icon">
+          <input class="form-control" type="text" name="icon" v-model="group.icon">
         </div>
         <div class="form-group">
-          <button>Submit</button>
+          <button class="btn btn-primary">Submit</button>
         </div>
       </form>
-      <form v-on:submit.prevent="submitSkill">
-        <caption>Add skill</caption>
+      <form v-on:submit.prevent="submitSkill" class="mb-5">
+        <h4>Add skill</h4>
         <div class="form-group">
           <label for="input">Skill:</label>
-          <input type="text" name="group-name" v-model="skill.title">
+          <input class="form-control" type="text" name="group-name" v-model="skill.title">
         </div>
         <div class="form-group">
           <label for="input">Value:</label>
-          <input type="text" name="value" v-model="skill.value">
+          <input class="form-control" type="text" name="value" v-model="skill.value">
         </div>
         <div class="form-group">
-          <select name="group" v-model="skill.group">
+          <select class="form-control" name="group" v-model="skill.group">
             <option v-for="(group, index) in groups" :key="index" :value="group._id">{{group.title}}</option>
           </select>
         </div>
         <div class="form-group">
-          <button>Submit</button>
+          <button class="btn btn-primary">Submit</button>
         </div>
       </form>
-      <form v-on:submit.prevent="submitAchievement">
-        <caption>Add achievement</caption>
+      <form v-on:submit.prevent="submitAchievement" class="mb-5">
+        <h4>Add achievement</h4>
         <div class="form-group">
           <label for="input">Title:</label>
-          <input type="text" name="group-name" v-model="achievement.title">
+          <input class="form-control" type="text" name="group-name" v-model="achievement.title">
         </div>
         <div class="form-group">
           <label for="input">Position:</label>
-          <input type="text" name="group-name" v-model="achievement.position">
+          <input class="form-control" type="text" name="group-name" v-model="achievement.position">
         </div>
         <div class="form-group">
           <label for="input">Period:</label>
-          <input type="text" name="group-name" v-model="achievement.period">
+          <input class="form-control" type="text" name="group-name" v-model="achievement.period">
         </div>
         <div class="form-group">
           <label for="input">Text:</label>
-          <textarea name="" id="" cols="30" rows="10" v-model="achievement.text"></textarea>
+          <textarea class="form-control" name="" id="" cols="30" rows="10" v-model="achievement.text"></textarea>
         </div>
         <div class="form-group">
-          <button>Submit</button>
+          <button class="btn btn-primary">Submit</button>
         </div>
       </form>
     </div>
