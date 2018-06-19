f1b36c640903f94754be70d31138dd8306de6804
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-06-19 10:52:54 +0300

    deploy to Heroku

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -11,7 +11,13 @@
       <h4 class="mb-1">О себе</h4>
       <p class="mb-5">~5 лет в веб разработке. Занимался приложениями для Facebook. Очень долго работал с Codeigniter. Сделал пару плагинов для Wordpress и Opencart.</p>
       <h4 class="mb-1">Что я ожидаю от работы?</h4>
-      <p class="mb-5">Профессиональный рост. Адекватный менеджмент. Возможность удаленной работы.
+      <p class="mb-5">
+        <ul class="expectations">
+          <li>Профессиональный рост.</li>
+          <li>Адекватный менеджмент.</li>
+          <li>Возможность удаленной работы.</li>
+          <li>Работа в команде</li>
+        </ul>  
       В данный момент интересует работа с Vue/Vuex (Варианты с React/Angular2+ тоже рассмотрю)</p>
       <ul class="list-inline list-social-icons mb-0">
         <li class="list-inline-item">
@@ -44,4 +50,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.expectations {
+  padding: 0;
+  margin-left: 1.25em;
+  li {
+    margin: 0;
+    padding: 0;
+  }
+}
 </style>
