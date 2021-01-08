7b5ffb7c9acd7a80cd6af1fb6d3e08fca9d98369
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2021-01-08 21:52:49 +0200

    deploy to Heroku

--- server/controllers/sendemail.controller.js
+++ server/controllers/sendemail.controller.js
@@ -1,37 +1,9 @@
 import nodemailer from 'nodemailer'
 
 class Sendmail {
-  constructor () {
-    // this.transporter = nodemailer.createTransport({
-    //   host: 'smtp.gmail.com',
-    //   auth: {
-    //     type: 'OAuth2',
-    //     user: process.env.GMAIL_USER,
-    //     clientId: process.env.GMAIL_CLIENT_ID,
-    //     clientSecret: process.env.GMAIL_CLIENT_SECRET
-    //   },
-    //   tls: {
-    //     rejectUnauthorized: false
-    //   }
-    // })
-    // this.mailOptions = {
-    //   from: process.env.GMAIL_USER,
-    //   to: process.env.MY_EMAIL,
-    //   subject: null,
-    //   html: null,
-    //   auth: {
-    //     user: process.env.GMAIL_USER,
-    //     refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-    //     accessToken: process.env.GMAIL_ACCESS_TOKEN,
-    //     expires: 1494388182480
-    //   }
-    // }
-  }
   async send (req, res) {
     try {
-      // const testAccount = await nodemailer.createTestAccount()
-
-      let transporter = nodemailer.createTransport({
+      const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
